37091fc2cb6153cccf85ccac97799cdb1390c3e6
nakashima <r.nakashima.a10@m.kyushu-u.ac.jp>
2023-05-02 15:43:02 +0900

    型ヒントを追加

--- laplace2d.py
+++ laplace2d.py
@@ -4,14 +4,15 @@ Chapra & Canale (2015) Example 29.3
 2 次元のラプラス方程式 (加熱された板の温度分布) を Gauss-Seidel 法で解く.
 Dirichlet 境界条件 or Neumann 境界条件, 熱源入り.
 
-T_{i,j} = ( T_{i+1,j}/(\delta x)^2 + T_{i-1,j}/(\delta x)^2
-    + T_{i,j+1}/(\delta y)^2 + T_{i,j-1}/(\delta y)^2
+T_{i,j} = ( T_{i+1,j}/(\\delta x)^2 + T_{i-1,j}/(\\delta x)^2
+    + T_{i,j+1}/(\\delta y)^2 + T_{i,j-1}/(\\delta y)^2
     + f_{i,j} )
-    / ( 2/(\delta x)^2 + 2/(\delta y)^2 )
+    / ( 2/(\\delta x)^2 + 2/(\\delta y)^2 )
 
-Parameters
+Notes
 -----
 コマンドライン引数以外のパラメータは以下に記述されている.
+
 """
 
 import logging
@@ -20,7 +21,6 @@ from time import perf_counter
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import rc
 
 # ========== パラメータ ==========
 # 境界条件
@@ -28,7 +28,7 @@ from matplotlib import rc
 # N: Neumann 境界条件 (dT/dx or dT/dy)
 T_TOP: tuple[float, str] = (100, 'D')
 T_RIGHT: tuple[float, str] = (50, 'D')
-T_BOTTOM: tuple[float, str] = (0, 'D')
+T_BOTTOM: tuple[float, str] = (0, 'N')
 T_LEFT: tuple[float, str] = (75, 'D')
 
 # 加速パラメータ
@@ -51,26 +51,26 @@ BOTTOM: float = 0
 LEFT: float = 0
 
 
-def SOURCE(x: float, y: float) -> float:
+def heat_source(pos_x: float, pos_y: float) -> float:
     """
     熱源を指定する関数
     """
 
-    f: float
+    heat: float
 
-    f = 0
+    heat = 0
 
     # center_x: float = (RIGHT-LEFT)/2
     # center_y: float = (TOP-BOTTOM)/2
     # sigma_x: float = (RIGHT-LEFT)/10
     # sigma_y: float = (TOP-BOTTOM)/10
     # norm: float = 1 / 2*math.pi*sigma_x*sigma_y
-    # f = 100000 * math.exp(
-    #     - (x-center_x)**2/(2*(sigma_x**2))
-    #     - (y-center_y)**2/(2*(sigma_y**2))
+    # heat = 100000 * math.exp(
+    #     - (pos_x-center_x)**2/(2*(sigma_x**2))
+    #     - (pos_y-center_y)**2/(2*(sigma_y**2))
     # ) * norm
 
-    return f
+    return heat
 #
 # ========== パラメータ [ここまで] ==========
 
@@ -78,35 +78,38 @@ def SOURCE(x: float, y: float) -> float:
 NUM_GRID_X: int = int((RIGHT-LEFT)/DELTA_X) - 1
 NUM_GRID_Y: int = int((TOP-BOTTOM)/DELTA_Y) - 1
 
-NUM_COL = NUM_GRID_X + 2
-NUM_ROW = NUM_GRID_Y + 2
+NUM_COL: int = NUM_GRID_X + 2
+NUM_ROW: int = NUM_GRID_Y + 2
 
-LIN_X = np.linspace(LEFT, RIGHT, NUM_COL)
-LIN_Y = np.linspace(BOTTOM, TOP, NUM_ROW)
+LIN_X: np.ndarray = np.linspace(LEFT, RIGHT, NUM_COL)
+LIN_Y: np.ndarray = np.linspace(BOTTOM, TOP, NUM_ROW)
 
+GRID_X: np.ndarray
+GRID_Y: np.ndarray
 GRID_X, GRID_Y = np.meshgrid(LIN_X, LIN_Y)
 
 
-def plot_temp(temp):
+def plot_temp(temp: np.ndarray):
     """
-    温度分布を図示する関数
+    温度分布を図示する
 
     Parameters
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度を保存しておく配列
 
     Return
     -----
     fig :
         figure オブジェクト
+
     """
 
     fig = plt.figure()
-    ax = fig.add_subplot(111)
+    axis = fig.add_subplot(111)
 
-    t_max = range_temp()[0]
-    t_min = range_temp()[1]
+    t_max: float = range_temp()[0]
+    t_min: float = range_temp()[1]
 
     t_step: float
     if t_max-t_min < 100:
@@ -114,30 +117,30 @@ def plot_temp(temp):
     else:
         t_step = 10
     #
-    levels = np.arange(t_min, t_max+1, t_step)
+    levels: np.ndarray = np.arange(t_min, t_max+1, t_step)
 
-    cf = ax.contourf(GRID_X, GRID_Y, temp,
-                     vmin=t_min, vmax=t_max,
-                     levels=levels, cmap='inferno')
+    contf = axis.contourf(GRID_X, GRID_Y, temp,
+                          vmin=t_min, vmax=t_max,
+                          levels=levels, cmap='inferno')
 
-    ax.set_xlim(LEFT, RIGHT)
-    ax.set_ylim(BOTTOM, TOP)
+    axis.set_xlim(LEFT, RIGHT)
+    axis.set_ylim(BOTTOM, TOP)
 
-    ax.set_xlabel(r'$x$', fontsize=18)
-    ax.set_ylabel(r'$y$', fontsize=18)
+    axis.set_xlabel(r'$x$', fontsize=18)
+    axis.set_ylabel(r'$y$', fontsize=18)
 
-    ax.tick_params(labelsize=16)
-    ax.minorticks_on()
+    axis.tick_params(labelsize=16)
+    axis.minorticks_on()
 
-    ax.set_aspect('equal')
+    axis.set_aspect('equal')
 
     fig.tight_layout()
 
     fig.subplots_adjust(right=0.91, wspace=0.25)
-    axpos = ax.get_position()
+    axpos = axis.get_position()
     cbar_ax = fig.add_axes([0.81, axpos.y0, 0.01, axpos.height])
 
-    cbar = fig.colorbar(cf, cax=cbar_ax)
+    cbar = fig.colorbar(contf, cax=cbar_ax)
     cbar.ax.tick_params(labelsize=16)
     cbar.set_label(label=r'$T [\mathrm{^\circ C}]$', size=18)
 
@@ -145,22 +148,23 @@ def plot_temp(temp):
 #
 
 
-def init_temp() -> tuple:
+def init_temp() -> tuple[np.ndarray, np.ndarray]:
     """
-    格子点の温度を保存しておく配列を作成する関数
+    格子点の温度を保存しておく配列を作成する
 
     Returns
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度を保存しておく配列
-    grid : ndarray
+    grid : np.ndarray
         計算する格子点の位置を保存しておく配列
+
     """
 
-    temp = np.full((NUM_ROW, NUM_COL), math.inf)
-    grid = np.full((NUM_ROW, NUM_COL), False)
+    temp: np.ndarray = np.full((NUM_ROW, NUM_COL), math.inf)
+    grid: np.ndarray = np.full((NUM_ROW, NUM_COL), False)
 
-    t_init = range_temp()[2]
+    t_init: float = range_temp()[2]
 
     for i_x in range(NUM_COL):
         for i_y in range(NUM_ROW):
@@ -220,7 +224,7 @@ def init_temp() -> tuple:
 
 def range_temp() -> tuple[float, float, float]:
     """
-    とりうる温度の範囲を決める関数
+    とりうる温度の範囲を決める
 
     Returns
     -----
@@ -230,6 +234,7 @@ def range_temp() -> tuple[float, float, float]:
         境界条件の温度の最小値
     t_mean : float
         境界条件の温度の平均値
+
     """
 
     t_bc: list = [
@@ -246,19 +251,20 @@ def range_temp() -> tuple[float, float, float]:
 
 def main_loop(temp, grid):
     """
-    Gauss-Seidel 法の反復を行う関数
+    Gauss-Seidel 法の反復を行う
 
     Parameters
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度 (初期値) を保存しておく配列
-    grid : ndarray
+    grid : np.ndarray
         計算する格子点の位置を保存しておく配列
 
     Returns
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度 (収束した値) を保存しておく配列
+
     """
 
     for i_itr in range(MAX_ITER):
@@ -277,26 +283,35 @@ def main_loop(temp, grid):
 #
 
 
-def wrapper_gauss_seidel(temp, grid) -> tuple:
+def wrapper_gauss_seidel(temp: np.ndarray, grid: np.ndarray) \
+        -> tuple[np.ndarray, bool, float]:
     """
-    Gauss-Seidel 法の反復 1 回分の計算を行う関数
+    Gauss-Seidel 法の反復 1 回分の計算を行う
 
     Parameters
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度を保存しておく配列
-    grid : ndarray
+    grid : np.ndarray
         計算する格子点の位置を保存しておく配列
 
     Returns
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度を保存しておく配列
     stop : bool
         停止基準を満たすかどうかのブール値
     max_error : float
         誤差の最大値
+
     """
+
+    t_old: float
+    t_up: float
+    t_right: float
+    t_down: float
+    t_left: float
+    t_new: float
 
     stop: bool = True
     max_error: float = 0
@@ -326,13 +341,13 @@ def wrapper_gauss_seidel(temp, grid) -> tuple:
 #
 
 
-def value_temp(temp, i_x: int, i_y: int) -> float:
+def value_temp(temp: np.ndarray, i_x: int, i_y: int) -> float:
     """
-    Neumann 境界条件のときに Gauss-Seidel 法の計算に使う格子点が, 境界外の点を含むかどうかをチェックする関数
+    Neumann 境界条件のときに Gauss-Seidel 法の計算に使う格子点が, 境界外の点を含むかどうかをチェックする
 
     Parameters
     -----
-    temp : ndarray
+    temp : np.ndarray
         格子点の温度を保存しておく配列
     i_x : int
         確認を行う格子点の番号 (x)
@@ -341,8 +356,9 @@ def value_temp(temp, i_x: int, i_y: int) -> float:
 
     Returns
     -----
-    t_value : ndarray
+    t_value : np.ndarray
         確認を行なった格子点での温度
+
     """
 
     t_value: float
@@ -364,9 +380,10 @@ def value_temp(temp, i_x: int, i_y: int) -> float:
 
 
 def gauss_seidel(
-        t_up: float, t_right: float, t_down: float, t_left: float) -> float:
+        t_up: float, t_right: float, t_down: float, t_left: float) \
+        -> float:
     """
-    上下左右の格子点の温度を使って, 真ん中の格子点の温度の候補値を求める関数
+    上下左右の格子点の温度を使って, 真ん中の格子点の温度の候補値を求める
 
     Parameters
     -----
@@ -383,12 +400,14 @@ def gauss_seidel(
     -----
     t_center : float
         真ん中の格子点の温度
+
     """
 
     dx2: float = DELTA_X ** 2
     dy2: float = DELTA_X ** 2
 
-    t_center = (t_up/dy2+t_right/dx2+t_down/dy2+t_left/dx2) / (2/dx2+2/dy2)
+    t_center: float \
+        = (t_up/dy2+t_right/dx2+t_down/dy2+t_left/dx2) / (2/dx2+2/dy2)
 
     return t_center
 #
@@ -408,15 +427,16 @@ def value_src(i_x: int, i_y: int) -> float:
     Return
     -----
         f_value : float
+
     """
 
-    pos_x = LEFT + i_x * DELTA_X
-    pos_y = BOTTOM + i_y * DELTA_Y
+    pos_x: float = LEFT + i_x * DELTA_X
+    pos_y: float = BOTTOM + i_y * DELTA_Y
 
     dx2: float = DELTA_X ** 2
     dy2: float = DELTA_X ** 2
 
-    f_value = SOURCE(pos_x, pos_y) / (2/dx2+2/dy2)
+    f_value: float = heat_source(pos_x, pos_y) / (2/dx2+2/dy2)
 
     return f_value
 #
@@ -424,7 +444,7 @@ def value_src(i_x: int, i_y: int) -> float:
 
 def overrelaxation(t_old: float, t_new: float) -> float:
     """
-    加速緩和を行う関数
+    加速緩和を行う
 
     Parameters
     -----
@@ -437,9 +457,10 @@ def overrelaxation(t_old: float, t_new: float) -> float:
     -----
     t_new_sor : float
         加速緩和後の温度
+
     """
 
-    t_new_sor = LAMBDA*t_new + (1-LAMBDA)*t_old
+    t_new_sor: float = LAMBDA*t_new + (1-LAMBDA)*t_old
 
     return t_new_sor
 #
@@ -448,7 +469,7 @@ def overrelaxation(t_old: float, t_new: float) -> float:
 def check_stop(t_old: float, t_new: float,
                stop: bool, max_error: float) -> tuple[bool, float]:
     """
-    加速緩和を行う関数
+    停止基準をチェックする
 
     Parameters
     -----
@@ -467,6 +488,7 @@ def check_stop(t_old: float, t_new: float,
         停止基準を満たすかどうかのブール値
     max_error : float
         誤差の最大値
+
     """
 
     error: float = math.fabs((t_new-t_old)/t_new)
@@ -489,19 +511,21 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    rc('text', usetex=True)
+    plt.rcParams['text.usetex'] = True
 
-    temp, grid = init_temp()
-    temp = main_loop(temp, grid)
+    temperature: np.ndarray
+    grid_points: np.ndarray
+    temperature, grid_points = init_temp()
+    temperature = main_loop(temperature, grid_points)
 
     np.set_printoptions(precision=5)
-    print(temp)
+    print(temperature)
 
-    fig = plot_temp(temp)
+    figure = plot_temp(temperature)
 
     TIME_ELAPSED: float = perf_counter() - TIME_INIT
     print(f'{__name__}: {TIME_ELAPSED:.5f} s')
 
-    fig.savefig("laplace2d.png", dpi=300)
+    figure.savefig("laplace2d.png", dpi=300)
     plt.show()
 #
